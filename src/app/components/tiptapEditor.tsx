58a8385e6a40af927b2d5abe47ab32cc0eb128ba
fatbugsbunny <verses.v20@gmail.com>
2025-10-05 17:54:41 +0200

    style: add text formating for editor and toolbar buttons

--- src/app/components/tiptapEditor.tsx
+++ src/app/components/tiptapEditor.tsx
@@ -1,19 +1,50 @@
-'use client';
+"use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
+import { TextStyleKit } from "@tiptap/extension-text-style";
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Toolbar from "./toolbar";
 
-export default function TiptapEditor(){
+const extensions = [TextStyleKit, StarterKit];
 
-    const editor = useEditor({
-        extensions: [StarterKit],
-        content: "<p>Hello World! 🌎</p>",
-        immediatelyRender: false,
-    });
-
-    return (
-        <div className="border p-4 rounded-lg">
-            <EditorContent editor={editor} />
-        </div>
-    );
+export default function TipTapEditor() {
+	const editor = useEditor({
+		extensions,
+		content: `
+<h2>
+  Hi there,
+</h2>
+<p>
+  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+</p>
+<ul>
+  <li>
+    That’s a bullet list with one …
+  </li>
+  <li>
+    … or two list items.
+  </li>
+</ul>
+<p>
+  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+</p>
+<blockquote>
+  Wow, that’s amazing. Good work! 👏
+  <br />
+  — Tiptap
+</blockquote>
+`,
+		immediatelyRender: false,
+		editorProps: {
+			attributes: {
+				class: "prose prose-sm sm:prose lg:prose-lg mx-auto focus:outline-none",
+			},
+		},
+	});
+	return (
+		<div>
+			{editor ? <Toolbar editor={editor} /> : null}
+			<EditorContent editor={editor} className={"max-w-none"} />
+		</div>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import TiptapEditor from "@/app/components/tiptapEditor";
 export default function Home() {
   return (
       <main className="p-10">
-          <h1 className="text-2xl font-bold mb-4">Tiptap in Next.js</h1>
           <TiptapEditor />
       </main>
   );
