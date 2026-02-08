e02a190a7198f277dff7b13b564af7f7d217563d
emilio1480 <emiliopulaj04@gmail.com>
2026-02-08 14:54:31 +0100

    feat: add token rotation for Google

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import "next-auth/jwt"
+import { DefaultSession } from "next-auth";
 export interface Tool {
     name: string;
     onClick: () => void;
@@ -19,4 +21,21 @@ export interface ListSubtopic {
 export interface User{
 	username:string,
 	password:string,
+}
+
+declare module "next-auth" {
+	interface Session extends DefaultSession{
+		id_token?: string;
+		error?: "RefreshTokenError";
+	}
+}
+
+declare module "next-auth/jwt" {
+	interface JWT {
+		id_token?: string;
+		access_token?: string;
+		expires_at?: number;
+		refresh_token?: string;
+		error?: "RefreshTokenError";
+	}
 }
