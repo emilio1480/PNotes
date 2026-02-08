2232319261a2f0f0e601356adb5706cb47a25b45
emilio1480 <emiliopulaj04@gmail.com>
2026-02-08 14:54:31 +0100

    feat: add token rotation for Google

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,67 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-export const {handlers, signIn, signOut, auth } = NextAuth({
-	providers: [Google],
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	basePath: "/api/auth",
+	providers: [Google({authorization: {params: {access_type: "offline", prompt: "consent"}}})],
 	callbacks: {
 		authorized: async ({ auth }) => {
 			// Logged in users are authenticated, otherwise redirect to login page
-			return !!auth
+			return !!auth;
 		},
+		jwt: async({ token, account }) => {
+			if(account){
+				return {
+					...token,
+					access_token: account.access_token,
+					id_token: account.id_token,
+					expires_at: account.expires_at,
+					refresh_token: account.refresh_token
+				}
+			}else if(Date.now() < token.expires_at! * 1000){
+				return token;
+			}else{
+				if(!token.refresh_token) throw new TypeError("Missing refresh_token");
+
+				try {
+					const response = await fetch("https://oauth2.googleapis.com/token", {
+						method: "POST",
+						body: new URLSearchParams({
+							client_id: process.env.AUTH_GOOGLE_ID!,
+							client_secret: process.env.AUTH_GOOGLE_SECRET!,
+							grant_type: "refresh_token",
+							refresh_token: token.refresh_token,
+						}),
+					});
+
+					const tokensOrError = await response.json();
+
+					if (!response.ok) throw tokensOrError;
+
+					const newTokens = tokensOrError as {
+						access_token: string;
+						id_token: string;
+						expires_in: number;
+						refresh_token?: string;
+					};
+
+					return {
+						...token,
+						access_token: newTokens.access_token,
+						id_token: newTokens.id_token,
+						expires_at: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+						refresh_token: newTokens.refresh_token ? newTokens.refresh_token : token.refresh_token,
+					};
+				} catch (error) {
+					console.error("Error refreshing access_token", error);
+					// If we fail to refresh the token, return an error so we can handle it on the page
+					token.error = "RefreshTokenError";
+					return token;
+				}
+			}
+		},
+		session: async({session, token}) => {
+			return {...session, id_token: token.id_token, error: token.error}
+		}
 	},
-})
+});
