da0ececd69fd4bcc35e7371fa526067af846b365
emilio1480 <emiliopulaj04@gmail.com>
2026-02-08 14:52:17 +0100

    fix: authentication paths don't get redirected

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
 export {auth as middleware} from "./auth"
+
+export const config = {
+	matcher: [
+		/*
+		 * Exclude:
+		 * - auth routes
+		 * - static files
+		 * - images
+		 */
+		"/((?!api/auth|_next/static|_next/image|favicon.ico).*)",
+	],
+};
