2aa754553e0f4aa26cf12e0851b13057993d472d
emilio1480 <emiliopulaj04@gmail.com>
2026-02-10 21:57:26 +0100

    feat: dropdown functionality

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,25 @@
+"use client"
+import {useState} from "react";
+import {Menu} from "lucide-react";
+import Toolbar from "@/app/components/toolbar";
+import LogoutButton from "@/app/components/logoutButton";
+import {Editor} from "@tiptap/core";
+
+export default function Navbar({editor}: Readonly<{ editor: Editor }>) {
+    const [isToolbarVisible, setIsToolbarVisible] = useState(true);
+
+    return (
+        <div className={"flex justify-between mt-1 mb-2"}>
+                <button onClick={() => setIsToolbarVisible(!isToolbarVisible)}
+                        className="hover:cursor-pointer px-3 py-2  h-1/2 text-gray-700  rounded-md hover:bg-gray-200 transition-colors">
+                    <Menu size={24}/>
+                </button>
+                <div className={"pl-10"}>
+                    <Toolbar editor={editor} isToolbarVisible={isToolbarVisible}/>
+                </div>
+            <div>
+                <LogoutButton/>
+            </div>
+        </div>
+    )
+}
--- src/app/components/tiptapEditor.tsx
+++ src/app/components/tiptapEditor.tsx
@@ -1,104 +1,102 @@
 "use client";
 
-import { TextStyleKit } from "@tiptap/extension-text-style";
-import { EditorContent, useEditor } from "@tiptap/react";
+import {TextStyleKit} from "@tiptap/extension-text-style";
+import {EditorContent, useEditor} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import Toolbar from "./toolbar";
-import { addSubtopic, addRootSubtopic, updateSubtopic } from "@/actions";
-import { useState } from "react";
-import { Table } from "@tiptap/extension-table";
+import {addSubtopic, addRootSubtopic, updateSubtopic} from "@/actions";
+import {useState} from "react";
+import {Table} from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
-import toast, { Toaster } from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
+import ActionButtons from "@/app/components/actionButtons";
+import Navbar from "@/app/components/navbar";
 
 const extensions = [
-	StarterKit,
-	TextStyleKit,
-	Table.configure({
-		resizable: false,
-	}),
-	TableRow,
-	TableHeader,
-	TableCell,
+    StarterKit,
+    TextStyleKit,
+    Table.configure({
+        resizable: false,
+    }),
+    TableRow,
+    TableHeader,
+    TableCell,
 ];
 
 export default function TipTapEditor({
-	content,
-	title,
-	id,
-	ifAddingSubtopic,
-}: Readonly<{
-	content?: string;
-	title?: string;
-	id: string;
-	ifAddingSubtopic?: boolean;
+                                         content,
+                                         title,
+                                         id,
+                                         addingSubtopic,
+                                     }: Readonly<{
+    content?: string;
+    title?: string;
+    id: string;
+    addingSubtopic?: boolean;
 }>) {
-	const editor = useEditor({
-		extensions,
-		content: JSON.parse(content || '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"----- Shkruani ketu -----"}]}]}'),
-		immediatelyRender: false,
-		editorProps: {
-			attributes: {
-				class: "prose prose-md sm:prose lg:prose-lg mx-auto focus:outline-none",
-			},
-		},
-	});
-	const [newTitle, setNewTitle] = useState(title);
 
-	if (!editor) return null;
+    const editor = useEditor({
+        extensions,
+        content: JSON.parse(content || '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Write here"}]}]}'),
+        immediatelyRender: false,
+        editorProps: {
+            attributes: {
+                class: "prose prose-md sm:prose lg:prose-lg mx-auto focus:outline-none",
+            },
+        },
+    });
 
-	const handleSubmit = async (formData: FormData) => {
-		const editorContent = JSON.stringify(editor.getJSON());
+    const [newTitle, setNewTitle] = useState(title);
 
-		formData.set("content", editorContent);
+    if (!editor) return null;
 
-		try {
-			if (id == "null") {
-				await addRootSubtopic(formData);
-				return;
-			}
+    const handleSubmit = async (formData: FormData) => {
+        const editorContent = JSON.stringify(editor.getJSON());
 
-			if (ifAddingSubtopic) {
-				await addSubtopic(formData);
-			} else {
-				await updateSubtopic(formData);
-			}
-		} catch (err) {
-			const error = err as Error;
-			if (error.message !== "NEXT_REDIRECT") {
-				toast.error(error.message);
-			}
-		}
-	};
+        formData.set("content", editorContent);
 
-	return (
-		<div>
-			<Toaster />
-			<form action={handleSubmit}>
-				<Toolbar editor={editor} />
+        try {
+            if (id == "null") {
+                await addRootSubtopic(formData);
+                return;
+            }
 
-				<input
-					className={"mb-5 w-full text-center text-2xl focus:outline-none"}
-					autoComplete={"off"}
-					name={"title"}
-					onChange={(e) => setNewTitle(e.target.value)}
-					value={newTitle}
-					placeholder={"Vendosni nje titull"}
-				/>
-				<EditorContent editor={editor} className={"max-w-none"} />
-				<input type={"hidden"} name={"content"} value="" />
-				<input type={"hidden"} name={"id"} value={id} />
-				<input type={"hidden"} name={"ifAddingSubtopic"} value={`${ifAddingSubtopic}`} />
+            if (addingSubtopic) {
+                await addSubtopic(formData);
+            } else {
+                await updateSubtopic(formData);
+            }
+        } catch (err) {
+            const error = err as Error;
+            throw new Error(error.message);
+        }
+    };
 
-				<input
-					className={"fixed bottom-4 z-10 ml-[50%] w-40 -translate-x-1/2 rounded-lg bg-yellow-500 font-[500] text-white transition-all ease-out hover:cursor-pointer hover:bg-yellow-600"}
-					type={"submit"}
-					value={"Siguro ndryshimet"}
-				/>
-			</form>
+    return (
+        <div className={"h-[100vh]"}>
+            <Toaster/>
+            <Navbar editor={editor}/>
+            <form action={handleSubmit}>
+                <input
+                    className={"mb-5 w-full text-center text-2xl focus:outline-none"}
+                    autoComplete={"off"}
+                    name={"title"}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                    value={newTitle}
+                    placeholder={"Add a title"}
+                />
+                <div className={"mx-auto"}>
+                    <div className={"overflow-y-auto scrollbar-none h-[75vh]"}>
+                        <EditorContent editor={editor} className={"max-w-none"} placeholder={"Type here"}/>
+                    </div>
+                </div>
+                <input type={"hidden"} name={"content"} value=""/>
+                <input type={"hidden"} name={"id"} value={id}/>
+                <input type={"hidden"} name={"ifAddingSubtopic"} value={`${addingSubtopic}`}/>
 
-			<div className={"fixed bottom-0 h-15 w-full border-t-1 border-gray-100 bg-white"} />
-		</div>
-	);
+                <ActionButtons id={id} addingSubtopic={addingSubtopic}/>
+            </form>
+        </div>
+    );
 }
