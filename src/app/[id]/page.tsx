7805043a5129350c8dfb6c366279df2a6f0bf2d9
emilio1480 <emiliopulaj04@gmail.com>
2026-02-10 22:15:23 +0100

    fix: actually remove unused buttons

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,5 @@
 import TipTapEditor from "@/app/components/tiptapEditor";
 import { getSubtopic } from "@/actions";
-import RedirectButton from "@/app/[id]/redirectButton";
-import DeleteButton from "@/app/[id]/deleteButton";
 
 export default async function Subtopic({ params }: Readonly<{ params: Promise<{ id: string }> }>) {
 	const { id } = await params;
@@ -9,13 +7,7 @@ export default async function Subtopic({ params }: Readonly<{ params: Promise<{ 
 
 	return (
 		<div>
-			<TipTapEditor id={id} content={subtopic.content} title={subtopic.title} ifAddingSubtopic={false} />
-			<RedirectButton
-				id={id}
-				text={"Shto nje neteme te re"}
-				className={"fixed bottom-4 left-0 z-10 ml-[50%] w-45 -translate-x-1/2 rounded-lg bg-blue-400 font-[500] text-white transition-all ease-out hover:cursor-pointer hover:bg-blue-500"}
-			/>
-			<DeleteButton id={id} />
+			<TipTapEditor id={id} content={subtopic.content} title={subtopic.title} addingSubtopic={false} />
 		</div>
 	);
 }
