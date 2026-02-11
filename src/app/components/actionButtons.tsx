04d81a3c8cff7178b8debb27cd02f10caa22296e
emilio1480 <emiliopulaj04@gmail.com>
2026-02-11 12:46:07 +0100

    style: switch button placements

--- src/app/components/actionButtons.tsx
+++ src/app/components/actionButtons.tsx
@@ -5,13 +5,20 @@ import { Save } from "lucide-react";
 export default function ActionButtons({ id, addingSubtopic }: Readonly<{ id: string; addingSubtopic?: boolean }>) {
 	return (
 		<div className={"mb-1/2 fixed right-0 bottom-1/2 grid translate-y-1/2 grid-cols-1 gap-y-10"}>
-			<button className={"z-10 w-max rounded-lg bg-gray-300 p-2 font-[500] text-gray-700 transition-all ease-out hover:cursor-pointer hover:bg-gray-600 hover:text-gray-100"} value={"Save"}>
+			<button
+				type={"submit"}
+				className={"z-10 w-max rounded-lg bg-gray-300 p-2 font-[500] text-gray-700 transition-all ease-out hover:cursor-pointer hover:bg-gray-600 hover:text-gray-100"}
+				value={"Save"}
+			>
 				<Save />
 			</button>
 			{!addingSubtopic && (
 				<>
+					<AddSubtopicButton
+						className={"z-10 w-max rounded-lg bg-gray-300 p-2 font-[500] text-gray-700 transition-all ease-out hover:cursor-pointer hover:bg-gray-600 hover:text-gray-100"}
+						id={id}
+					/>
 					<DeleteSubtopicButton id={id} />
-					<AddSubtopicButton className={"z-10 w-max rounded-lg bg-gray-300 p-2 font-[500] text-gray-700 transition-all ease-out hover:cursor-pointer hover:bg-gray-600 hover:text-gray-100"} id={id} />
 				</>
 			)}
 		</div>
